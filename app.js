1a88200f4af4253c60a7a5a42d04c59531d3fc24
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-10-14 18:29:43 +0530

    slide animation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function animateSlides() {
 
   const sliders = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const revealimg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealtext = slide.querySelector(".reveal-text");
@@ -18,6 +18,38 @@ function animateSlides() {
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1.5");
     slideTl.fromTo(revealtext, { x: "0%" }, { x: "100%" }, "-=0.75");
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.3");
+
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideTl)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(Controller);
+    const pageT1 = gsap.timeline();
+    let nextSlide = slides.lenght - 1 === index ? "end" : slides[index + 1];
+    pageT1.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageT1.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0 });
+    pageT1.fromTo(nextSlide, { y: "50%" }, { y: "0%" });
+
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageT1)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(Controller);
   });
 }
 animateSlides();
